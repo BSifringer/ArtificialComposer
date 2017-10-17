390367f350a4353347d6c3eb8ca7b6639c2afaf9
aschethor <aschethor@hotmail.de>
2017-10-18 00:07:11 +0200

    batchify bug resolved

--- reber_grammar_playground.py
+++ reber_grammar_playground.py
@@ -27,28 +27,17 @@ decategorized = np.argmax(categorized,axis=1)
 
 decoded = ''.join([indices_char[i] for i in decategorized])
 
-#create batches
-num_batches = 10
-def create_dataset(dataset, look_back=1):
-	dataX, dataY = [], []
-	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
-		dataX.append(a)
-		dataY.append(dataset[i + look_back, 0])
-	return np.array(dataX), np.array(dataY)
-
-
 def batchify(X,Y,num_batches,batch_size,batch_length):
 	retX = np.ndarray(shape=np.append([num_batches,batch_size,batch_length],X.shape[1:]))
 	retY = np.ndarray(shape=np.append([num_batches,batch_size],Y.shape[1:]))
 	for i in range(num_batches):
 		for j in range(batch_size):
 			for k in range(batch_length):
-				retX[i][j][k]=X[i+j*batch_length+k]
-			retY[i][j]=Y[i+j*batch_length+1]
+				retX[i][j][k]=X[j+i*batch_length+k]
+			retY[i][j]=Y[j+(i+1)*batch_length-1]
 	return retX,retY
 
 batch_X,batch_Y = batchify(categorized,categorizedY,10,10,10)
 
-blubb=0#
+blubb=0 #mark breakpoint to debug ;)
 exit(0)
